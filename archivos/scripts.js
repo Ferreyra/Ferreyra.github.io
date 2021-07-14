1ff8196a4a9a0d4ff0a6696098d0bc6694db4fee
Ferreyra <robfercor@gmail.com>
2021-07-14 17:37:11 -0500

    Update scripts.js

--- archivos/scripts.js
+++ archivos/scripts.js
@@ -20,7 +20,8 @@ function initClient() {
   // Initialize the gapi.client object, which app uses to make API requests.
   // Get API key and client ID from API Console.
   // 'scope' field specifies space-delimited list of access scopes.
-  var idaw = document.getElementById('gpcid').value + ".apps.googleusercontent.com"
+  // var idaw = document.getElementById('gpcid').value + ".apps.googleusercontent.com"
+  var idaw = "38796529945-7a6au1vu0ntr565rigmrb0bju1t5nu7k" + ".apps.googleusercontent.com"
   gapi.client.init({
       'clientId': idaw,
       'discoveryDocs': [discoveryUrl],
