fb6c12d41e44055f8bff2531d2744869860c305b
RFerreyra <robfercor@gmail.com>
2021-08-25 17:57:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE    = 'static-v1';
+/*  const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -62,4 +62,4 @@ self.addEventListener( 'fetch', e => {
       }
   });
   e.respondWith( respuesta );
-});
+});   */
